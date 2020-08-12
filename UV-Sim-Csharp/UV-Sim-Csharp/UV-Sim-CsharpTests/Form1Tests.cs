bea1b49203a28488d8a6adb802b286322b93178f
Cory <10764424@my.uvu.edu>
2020-08-12 03:09:29 -0600

    FInal Working with UnitTest
    added multiple process and unit tests

--- UV-Sim-Csharp/UV-Sim-Csharp/UV-Sim-CsharpTests/Form1Tests.cs
+++ UV-Sim-Csharp/UV-Sim-Csharp/UV-Sim-CsharpTests/Form1Tests.cs
@@ -17,7 +17,65 @@ namespace UV_Sim_Csharp.Tests
             Form1 _Form1 = new Form1();
             var temp = _Form1.Memory;
             int[] Compare = new int[1000];
-        Assert.AreEqual(temp.GetType(), Compare.GetType());
+            Assert.AreEqual(temp.GetType(), Compare.GetType());
+        }
+        [TestMethod]
+        public void TestConvertBinary()
+        {
+            Form1 _Form1 = new Form1();
+            string a = _Form1.ConvertBinary(201);
+            string answer = "11001001";
+            Assert.AreEqual(a, answer);
+        }
+        [TestMethod]
+        public void TestBinaryAdd()
+        {
+            Form1 _Form1 = new Form1();
+            string a = _Form1.ConvertBinary(1001);
+            string b = _Form1.ConvertBinary(1005);
+            string c = _Form1.BinaryAdd(a, b);
+            string answer = "0101010110";
+            Assert.AreEqual(c, answer);
+        }
+        [TestMethod]
+        public void TestBinarySubtract()
+        {
+            Form1 _Form1 = new Form1();
+            string a = _Form1.ConvertBinary(1001);
+            string b = _Form1.ConvertBinary(1005);
+            string c = _Form1.BinarySub(a, b);
+            string answer = "11111111";
+            Assert.AreEqual(c, answer);
+        }
+        [TestMethod]
+        public void TestBinaryMultiply()
+        {
+            Form1 _Form1 = new Form1();
+            int a = 1001;
+            int b = 1005;
+            int c = _Form1.BinaryMulti(a, b);
+            int answer = 466;
+            Assert.AreEqual(c, answer);
+        }
+        [TestMethod]
+        public void TestBinaryDivide()
+        {
+            Form1 _Form1 = new Form1();
+            int a = 1001;
+            int b = 1005;
+            int c = _Form1.BinaryDivide(a, b);
+            int answer = 1;
+            Assert.AreEqual(c, answer);
+        }
+        [TestMethod]
+        public void TestBinaryMod()
+        {
+            Form1 _Form1 = new Form1();
+            int a = 1001;
+            int b = 1005;
+            int c = _Form1.BinaryMod(a, b);
+            int answer = -4;
+            Assert.AreEqual(c, answer);
         }
     }
 }
